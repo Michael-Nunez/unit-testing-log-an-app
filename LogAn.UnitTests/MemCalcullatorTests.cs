92f6253f66de3ad907c189500c946d637f587391
Michael Nunez <michael.nunez@uepa.com>
2024-04-15 16:50:48 -0400

    test: MemCalcullator -> Add_WhenCalled_ChangesSum()

--- LogAn.UnitTests/MemCalcullatorTests.cs
+++ LogAn.UnitTests/MemCalcullatorTests.cs
@@ -6,9 +6,25 @@
         [Test]
         public void Sum_ByDefault_ReturnsZero()
         {
-            var calc = new MemCalcullator();
+            var calc = MakeCalc();
             int lastSum = calc.Sum();
+
             Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            var calc = MakeCalc();
+            calc.Add(1);
+            int sum = calc.Sum();
+
+            Assert.AreEqual(1, sum);
+        }
+
+        private MemCalcullator MakeCalc()
+        {
+            return new MemCalcullator();
         }
     }
 }
